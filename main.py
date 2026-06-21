3eb60f9a50677c036f53f3d4d916a9d64040b793
nazar2009it <148614753+nazar2009it@users.noreply.github.com>
2026-06-22 01:31:45 +0600

    dz2-3

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import flet as ft
 from datetime import datetime
 
-name = input("Введите имя: ")
 
-current_time = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
+def main(page: ft.Page):
+    page.title = "Homework 3"
 
-print(f"{current_time} - Привет, {name}!")
+    name_field = ft.TextField(label="Введите имя")
+
+    result_text = ft.Text()
+
+    def show_message(e):
+        current_time = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
+        result_text.value = f"{current_time} - Привет, {name_field.value}!"
+        page.update()
+
+    page.add(
+        name_field,
+        ft.ElevatedButton("Показать", on_click=show_message),
+        result_text
+    )
+
+
+ft.app(target=main)
